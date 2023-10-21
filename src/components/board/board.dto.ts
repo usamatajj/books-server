72a2707f611f2e41c7d4f1da3cbdd05eade2e0c8
Adnan Khan <adnankhannad@gmail.com>
2023-10-21 20:18:22 +0500

    Add Field into Board, Class, Book

--- src/components/board/board.dto.ts
+++ src/components/board/board.dto.ts
@@ -1,54 +1,68 @@
 import {
-    IsNotEmpty,
-    IsEmail,
-    Matches,
-    IsString,
-    IsOptional,
-    IsLowercase,
-    IsBoolean,
-    IsArray,
-    IsIn
+  IsNotEmpty,
+  IsEmail,
+  Matches,
+  IsString,
+  IsOptional,
+  IsLowercase,
+  IsBoolean,
+  IsArray,
+  IsIn,
 } from 'class-validator';
 import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
 
-
 export class AddBoardDto {
-    @ApiProperty({ example: 'Punjab' })
-    @IsNotEmpty()
-    @IsString()
-    boardName: string
+  @ApiProperty({ example: 'Punjab' })
+  @IsNotEmpty()
+  @IsString()
+  boardName: string;
 
-    @ApiProperty({ example: 'base64' })
-    @IsNotEmpty()
-    @IsString()
-    image: string
+  @ApiProperty({ example: 'base64' })
+  @IsNotEmpty()
+  @IsString()
+  image: string;
 
+  @ApiProperty({ example: 'This is test board' })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty({ example: 'The Punjab' })
+  @IsNotEmpty()
+  @IsString()
+  region: string;
 }
 
 export class UpdateBoardDto {
-    @ApiProperty({ example: '_id here' })
-    @IsNotEmpty()
-    @IsString()
-    _id: string
+  @ApiProperty({ example: '_id here' })
+  @IsNotEmpty()
+  @IsString()
+  _id: string;
 
-    @ApiProperty({ example: 'Punjab' })
-    @IsNotEmpty()
-    @IsString()
-    boardName: string
+  @ApiProperty({ example: 'Punjab' })
+  @IsNotEmpty()
+  @IsString()
+  boardName: string;
 
-    @ApiProperty({ example: 'base64' })
-    @IsNotEmpty()
-    @IsString()
-    image: string
+  @ApiProperty({ example: 'base64' })
+  @IsNotEmpty()
+  @IsString()
+  image: string;
 
+  @ApiProperty({ example: 'This is test board' })
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @ApiProperty({ example: 'The Punjab' })
+  @IsNotEmpty()
+  @IsString()
+  region: string;
 }
 
 export class DeleteBoardDto {
-    @ApiProperty({ example: '_id here' })
-    @IsNotEmpty()
-    @IsString()
-    _id: string
-
+  @ApiProperty({ example: '_id here' })
+  @IsNotEmpty()
+  @IsString()
+  _id: string;
 }
-
-
